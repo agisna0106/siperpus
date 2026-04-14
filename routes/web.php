e771ba9b1752b4f97b6b90f95c418af3a93b0c96
Agis <sarifan176@gmail.com>
2026-04-14 16:15:13 +0700

    menampilkan fullname di vies/greeting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('greeting', function() {
+    $user = User::first();
+    return view('greeting', compact('user'));
 });
